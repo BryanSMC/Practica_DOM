a29d4459a7be6fbcabcc443060191fb43a9a3f28
Bryan Steven <bryan.s.munozc@gmail.com>
2023-09-02 16:37:35 -0500

    Manipulando las clases con DOM para agregar el funcionamiento del menu

--- js/menu_hamburguesa.js
+++ js/menu_hamburguesa.js
@@ -1,12 +1,18 @@
-export default function hamburgerMenu(menuBtn, menuB) {
+export default function hamburgerMenu(menuBtn, menuB, menuLink) {
     const d = document;
 
-    const buttonMenu = d.querySelector(menuBtn);
-    const menu = d.querySelector(menuB)
+    const $buttonMenu = d.querySelector(menuBtn);
+    const $menu = d.querySelector(menuB)
+    const $menuLink = d.querySelector(menuLink)
 
-    buttonMenu.addEventListener("click", () => {
-        menu.classList.toggle("is-active");
-        buttonMenu.classList.toggle("is-active");
+    $buttonMenu.addEventListener("click", () => {
+        $menu.classList.toggle("is-active");
+        $buttonMenu.classList.toggle("openmenu")
+    })
+
+    $menuLink.addEventListener("click", (e) => {
+        $menu.classList.remove("is-active");
+        $buttonMenu.classList.remove("openmenu");
     })
 }
 
