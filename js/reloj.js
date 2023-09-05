39237bfc879466dd05e61e7f4b505ba7552b7c30
Bryan Steven <bryan.s.munozc@gmail.com>
2023-09-05 14:13:59 -0500

    Agregando funcion de agregado

---
+++ js/reloj.js
@@ -0,0 +1,54 @@
+/* Exports */
+export { startTime, finalizarReloj }
+
+/* Imports */
+import { $formato, $reloj } from "./index_dom.js";
+
+let intervalo;
+
+function startTime() {
+
+    function currentTime() {
+        console.log("La función currentTime se está ejecutando.");
+
+        let date = new Date();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let second = date.getSeconds();
+
+        hour = (hour < 10) ? "0" + hour : hour;
+        minute = (minute < 10) ? "0" + minute : minute;
+        second = (second < 10) ? "0" + second : second;
+
+        /* Formato 12h */
+
+        if (hour >= 12) {
+            hour = hour - 12;
+            var ampm = `PM`
+        } else {
+            ampm = `AM`
+        }
+
+        if (hour <= 12) {
+            hour = `0${hour}`
+        }
+
+        if (hour === 0) {
+            hour = 12;
+        }
+
+        let time = `${hour}:${minute}:${second}`;
+        let formato = `${ampm}`
+
+        $reloj.innerHTML = time;
+        $formato.innerHTML = formato;
+    }
+
+    intervalo = setInterval(currentTime, 1000);
+}
+
+function finalizarReloj() {
+    clearInterval(intervalo);
+    $reloj.innerHTML = "";
+    $formato.innerHTML = "";
+}
