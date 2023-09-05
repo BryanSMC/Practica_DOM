1a4610fea1c12e24ea0c8cdb2557b88ed6910283
Bryan Steven <bryan.s.munozc@gmail.com>
2023-09-05 17:14:30 -0500

    Agregando obtencion de Alarma sonora

--- js/index_dom.js
+++ js/index_dom.js
@@ -4,6 +4,7 @@ export { $reloj, $formato }
 /* Imports */
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { finalizarReloj, startTime } from "./reloj.js";
+import { sound } from "./alarma_sonora.js";
 
 /* Obteniendo para el menu */
 const d = document;
@@ -32,5 +33,26 @@ $fin.addEventListener("click", (e) => {
     $inicio.disabled = false;
     $inicio.classList.remove("no-hover");
 });
+
+/* Obteniendo para la alarma */
+
+let $alarma = d.getElementById("alarma"),
+    $reproducir = d.getElementById("reproducir-alarma"),
+    $pausar = d.getElementById("pausar-alarma");
+
+$reproducir.addEventListener("click", () => {
+    sound.play();
+    sound.loop = true;
+    $reproducir.disabled = true;
+    $reproducir.classList.add("no-hover");
+});
+
+$pausar.addEventListener("click", () => {
+    sound.pause();
+    sound.currentTime = 0;
+    $reproducir.disabled = false;
+    $reproducir.classList.remove("no-hover");
+})
+
 
 console.log("El archivo js/index_dom.js se está ejecutando correctamente.");
