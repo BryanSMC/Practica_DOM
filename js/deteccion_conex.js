21fa5f25046a3045cdf7bd0024e4022d0b5f021c
Bryan Steven <bryan.s.munozc@gmail.com>
2023-09-15 13:30:42 -0500

    Agregando: Fichero con funcionalidad de Detección de conexión

---
+++ js/deteccion_conex.js
@@ -0,0 +1,32 @@
+let d = document,
+    w = window,
+    n = navigator;
+
+function detectCon() {
+    const online = () => {
+        const $element = d.createElement("div")
+
+        if (navigator.onLine) {
+            $element.textContent = "Conexión Reestablecida";
+            $element.classList.add("online")
+            $element.classList.remove("offline")
+        } else {
+            $element.textContent = "Conexión Perdida";
+            $element.classList.add("offline")
+            $element.classList.remove("online")
+        }
+
+        d.body.insertAdjacentElement("afterbegin", $element);
+        setTimeout(() => d.body.removeChild($element), 2000);
+    }
+
+    w.addEventListener("online", (e) => {
+        online();
+    })
+
+    w.addEventListener("offline", (e) => {
+        online();
+    })
+}
+
+export { detectCon }
