810a1df72198520538aea666a5d835c310313170
Bryan Steven <bryan.s.munozc@gmail.com>
2023-09-16 12:37:49 -0500

    Actualizando: Codigo

---
+++ js/deteccion_webcam.js
@@ -0,0 +1,22 @@
+const d = document,
+    n = navigator;
+
+function webcam(cam) {
+    const $cam = d.getElementById(cam)
+
+    if (n.mediaDevices.getUserMedia) {
+        n.mediaDevices.getUserMedia({ audio: false, video: true })
+            .then((stream) => {
+                $cam.srcObject = stream;
+                $cam.play();
+            })
+            .catch((err) => {
+                $cam.insertAdjacentHTML("afterend", `<p class"center-items">Sucedio un error con la camara o sus permisos</p>
+                <p><mark>${err}</mark></p>`)
+                alert(`Sucedio un error con la camara o sus permisos ${err}`);
+            })
+
+    }
+}
+
+export { webcam }
--- js/menu_hamburguesa.js
+++ js/menu_hamburguesa.js
@@ -1,21 +1,18 @@
-export default function hamburgerMenu(menuBtn, menuB, menuLink, scroll) {
+export default function hamburgerMenu(menuBtn, menuB, menuLink) {
     const d = document;
 
     const $buttonMenu = d.querySelector(menuBtn),
         $menu = d.querySelector(menuB),
-        $menuLink = d.querySelector(menuLink),
-        $scrollH = d.getElementById(scroll);
+        $menuLink = d.querySelector(menuLink);
 
     $buttonMenu.addEventListener("click", () => {
         $menu.classList.toggle("is-active");
         $buttonMenu.classList.toggle("openmenu");
-        $scrollH.classList.toggle("menu-scroll-hidden");
     })
 
     $menuLink.addEventListener("click", (e) => {
         $menu.classList.remove("is-active");
         $buttonMenu.classList.remove("openmenu");
-        $scrollH.classList.remove("menu-scroll-hidden");
     })
 }
 
