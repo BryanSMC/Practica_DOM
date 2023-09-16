7155f0e36ce7eedb8bd422aaa34e663bdc892401
Bryan Steven <bryan.s.munozc@gmail.com>
2023-09-16 10:47:05 -0500

    Agregando: Scroll Hidden al evento click del boton menu

--- js/menu_hamburguesa.js
+++ js/menu_hamburguesa.js
@@ -1,18 +1,21 @@
-export default function hamburgerMenu(menuBtn, menuB, menuLink) {
+export default function hamburgerMenu(menuBtn, menuB, menuLink, scroll) {
     const d = document;
 
-    const $buttonMenu = d.querySelector(menuBtn);
-    const $menu = d.querySelector(menuB)
-    const $menuLink = d.querySelector(menuLink)
+    const $buttonMenu = d.querySelector(menuBtn),
+        $menu = d.querySelector(menuB),
+        $menuLink = d.querySelector(menuLink),
+        $scrollH = d.getElementById(scroll);
 
     $buttonMenu.addEventListener("click", () => {
         $menu.classList.toggle("is-active");
-        $buttonMenu.classList.toggle("openmenu")
+        $buttonMenu.classList.toggle("openmenu");
+        $scrollH.classList.toggle("menu-scroll-hidden");
     })
 
     $menuLink.addEventListener("click", (e) => {
         $menu.classList.remove("is-active");
         $buttonMenu.classList.remove("openmenu");
+        $scrollH.classList.remove("menu-scroll-hidden");
     })
 }
 
